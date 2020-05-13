1f793a8186f6eaf810f2bffda92bc490ce5fd32f
ACPG-KO503P\Administrator <gpseaver@qq.com>
2020-05-13 20:38:59 +0800

    test home

--- src/test/java/com/wc/bootdemo/HelloTests.java
+++ src/test/java/com/wc/bootdemo/HelloTests.java
@@ -24,7 +24,7 @@ public class HelloTests {
 
     @Before
     public void setUp()  {
-        String s = "4";
+        String s = "5";
         mvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
     }
 
